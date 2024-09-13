dc6725b2e373b560a0de37b80ddbed3d002e6cb9
Matěj Korvas <matej@ibis.ai>
2024-09-13 17:59:40 +0200

    Use a shallow copy of metadata

--- tests/pipeline/test_deduce.py
+++ tests/pipeline/test_deduce.py
@@ -393,11 +393,27 @@ class TestDeduce:
         assert deid.deidentified_text == want
 
     def test_unrecognized_patient(self, model):
+        # Due to an indexing error, mentions following a single-character token (\n
+        # in this case) used to be handled wrongly.
         metadata = {"patient": Person(first_names=["Jan", "Jacob"],
                                       surname="Zeppelin")}
         doc = ("Deze patiënt(e)\n\nZeppelin Jan Jacob\n\nNiet in staat is van te "
                "werken.")
         want = ("Deze patiënt(e)\n\n[PATIENT]\n\nNiet in staat is van te werken.")
 
+        deid = model.deidentify(doc, metadata=metadata)
+        assert deid.deidentified_text == want
+
+    def test_unrecognized_patient_2(self, model):
+        metadata = {"patient": Person(first_names=["ANAÏS-MARÍA", "MAREIKE"],
+                                      surname="LAATST")}
+
+        doc = "Uw patiente ANAÏS-MARÍA LAATST is volledig gezond."
+        want = "Uw patiente [PATIENT] is volledig gezond."
+        deid = model.deidentify(doc, metadata=metadata)
+        assert deid.deidentified_text == want
+
+        doc = "Uw patiente ANAÏS-MARÍA LAATST is volledig gezond."
+        want = "Uw patiente [PATIENT] is volledig gezond."
         deid = model.deidentify(doc, metadata=metadata)
         assert deid.deidentified_text == want
